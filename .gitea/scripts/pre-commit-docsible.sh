a3dc5cb8347c31b47dad5e97511853ebc984ace5
Bertrand Lanson <bertrand.lanson@protonmail.com>
2025-06-25 22:49:21 +0200

    ci: adjust pre-commit docsible script to pick up freshly added files

--- .gitea/scripts/pre-commit-docsible.sh
+++ .gitea/scripts/pre-commit-docsible.sh
@@ -1,15 +1,21 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-role_dirs=$(git ls-tree -d --name-only HEAD roles/)
+role_dirs=$(git diff --cached --name-only --diff-filter=ACM | grep '^roles/' || true | cut -d/ -f1-2 | sort -u)
 
 checked_roles=()
 
 for role in $role_dirs; do
-  echo "Running docsible on $role"
-  docsible --role "$role" --no-backup
-  checked_roles+=("$role")
+  if [ -d "$role" ]; then
+    echo "Running docsible on $role"
+    docsible --role "$role" --no-backup
+    checked_roles+=("$role")
+  fi
 done
 
-echo "Checking for diffs in updated roles..."
-git diff --exit-code -- ${checked_roles[@]}
+if [ ${#checked_roles[@]} -gt 0 ]; then
+  echo "Checking for diffs in updated roles..."
+  git diff --exit-code -- ${checked_roles[@]}
+else
+  echo "No roles to check."
+fi
